2779821c91a15370b591d28ee00c04c37f57836f
Alishan Ladhani <alishan.ladhani@gmail.com>
2020-01-04 01:23:10 -0500

    Remove logging when colour change is successful

--- main.go
+++ main.go
@@ -65,11 +65,9 @@ func changeRoleColour(s *discordgo.Session, guildId string, roleId string) error
 	rand.Seed(time.Now().Unix())
 	colour := rand.Intn(16777216)
 
-	updatedRole, err := s.GuildRoleEdit(guildId, role.ID, role.Name, colour, role.Hoist, role.Permissions, role.Mentionable)
+	_, err = s.GuildRoleEdit(guildId, role.ID, role.Name, colour, role.Hoist, role.Permissions, role.Mentionable)
 	if err != nil {
 		return fmt.Errorf(fmt.Sprintf("error updating role colour for guild %s :", guildId), err)
-	} else {
-		fmt.Println(fmt.Sprintf("Changed colour from %s to %s. Actual: %s", role.Color, colour, updatedRole.Color))
 	}
 
 	return nil
