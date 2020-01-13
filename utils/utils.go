842af462a5a26a2fab10cea98650edb2c317b410
Alishan Ladhani <alishan.ladhani@gmail.com>
2020-01-12 23:52:24 -0500

    Remove unneeded error

--- utils/utils.go
+++ utils/utils.go
@@ -14,21 +14,21 @@ func FindOrCreateRole(s *discordgo.Session, guildId string) (*discordgo.Role, er
 		return nil, fmt.Errorf("error getting roles for guild %s: %w", guildId, err)
 	}
 
-	role, err := findRoleByName(roles)
-	if err != nil {
+	role := findRoleByName(roles)
+	if role == nil {
 		// TODO: Create role if it does not already exist
-		return nil, fmt.Errorf("error finding rainbow role for guild %s: %w", guildId, err)
+		return nil, fmt.Errorf("error finding rainbow role for guild %s", guildId)
 	}
 
 	return role, nil
 }
 
-func findRoleByName(roles []*discordgo.Role) (*discordgo.Role, error) {
+func findRoleByName(roles []*discordgo.Role) *discordgo.Role {
 	for _, role := range roles {
 		if roleNameRegex.MatchString(role.Name) {
-			return role, nil
+			return role
 		}
 	}
 
-	return nil, fmt.Errorf("could not find matching role")
+	return nil
 }
