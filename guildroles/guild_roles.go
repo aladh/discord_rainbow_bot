41e344a098fd52f3024e4dd3a77ab73538d4262f
Alishan Ladhani <alishan.ladhani@gmail.com>
2020-01-18 14:43:46 -0500

    Remove dependency on utils
    Causing circular dependency issue

--- guildroles/guild_roles.go
+++ guildroles/guild_roles.go
@@ -2,9 +2,10 @@ package guildroles
 
 import (
 	"fmt"
-	"github.com/ali-l/discord_rainbow_bot/utils"
 	"github.com/bwmarrin/discordgo"
 )
+
+const roleName = "Rainbow"
 
 type GuildRole struct {
 	GuildId string
@@ -50,7 +51,7 @@ func syncGuilds(session *discordgo.Session) error {
 	}
 
 	for _, guild := range guilds {
-		role, err := utils.FindOrCreateRole(session, guild.ID)
+		role, err := findOrCreateRole(session, guild.ID)
 		if err != nil {
 			return err
 		}
@@ -73,4 +74,46 @@ func onGuildDelete(session *discordgo.Session, guildDelete *discordgo.GuildDelet
 	if err != nil {
 		panic(fmt.Sprintf("error handling guildDelete ID %s: %s", guildDelete.ID, err))
 	}
+}
+
+func findOrCreateRole(s *discordgo.Session, guildId string) (*discordgo.Role, error) {
+	roles, err := s.GuildRoles(guildId)
+	if err != nil {
+		return nil, fmt.Errorf("error getting roles for guild %s: %w", guildId, err)
+	}
+
+	role := findRoleByName(roles)
+
+	if role == nil {
+		role, err = createRole(s, guildId)
+		if err != nil {
+			return nil, fmt.Errorf("error creating role for guild %s: %w", guildId, err)
+		}
+	}
+
+	return role, nil
+}
+
+func findRoleByName(roles []*discordgo.Role) *discordgo.Role {
+	for _, role := range roles {
+		if role.Name == roleName {
+			return role
+		}
+	}
+
+	return nil
+}
+
+func createRole(session *discordgo.Session, guildId string) (*discordgo.Role, error) {
+	role, err := session.GuildRoleCreate(guildId)
+	if err != nil {
+		return nil, fmt.Errorf("error creating role for guild %s: %w", guildId, err)
+	}
+
+	role, err = session.GuildRoleEdit(guildId, role.ID, roleName, role.Color, role.Hoist, role.Permissions, role.Mentionable)
+	if err != nil {
+		return role, fmt.Errorf("error updating name for guild ID %s: %w", guildId, err)
+	}
+
+	return role, nil
 }
--- utils/utils.go
+++ utils/utils.go
@@ -6,26 +6,6 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
-const roleName = "Rainbow"
-
-func FindOrCreateRole(s *discordgo.Session, guildId string) (*discordgo.Role, error) {
-	roles, err := s.GuildRoles(guildId)
-	if err != nil {
-		return nil, fmt.Errorf("error getting roles for guild %s: %w", guildId, err)
-	}
-
-	role := findRoleByName(roles)
-
-	if role == nil {
-		role, err = createRole(s, guildId)
-		if err != nil {
-			return nil, fmt.Errorf("error creating role for guild %s: %w", guildId, err)
-		}
-	}
-
-	return role, nil
-}
-
 func AssignRoleToSelf(session *discordgo.Session) {
 	guildroles.Run(func(guildRoles guildroles.GuildRoles) {
 		userID := session.State.User.ID
@@ -37,28 +17,4 @@ func AssignRoleToSelf(session *discordgo.Session) {
 			}
 		}
 	})
-}
-
-func findRoleByName(roles []*discordgo.Role) *discordgo.Role {
-	for _, role := range roles {
-		if role.Name == roleName {
-			return role
-		}
-	}
-
-	return nil
-}
-
-func createRole(session *discordgo.Session, guildId string) (*discordgo.Role, error) {
-	role, err := session.GuildRoleCreate(guildId)
-	if err != nil {
-		return nil, fmt.Errorf("error creating role for guild %s: %w", guildId, err)
-	}
-
-	role, err = session.GuildRoleEdit(guildId, role.ID, roleName, role.Color, role.Hoist, role.Permissions, role.Mentionable)
-	if err != nil {
-		return role, fmt.Errorf("error updating name for guild ID %s: %w", guildId, err)
-	}
-
-	return role, nil
 }
