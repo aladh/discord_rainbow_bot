7018e98ff54e4114643fbf66a89d6126d303807a
Alishan Ladhani <git@alad.ca>
2022-06-04 15:07:55 -0400

    Surface more errors when color is not updated
    - Add modified guildRoleEdit from discordgo that returns an error when the request fails

--- colors/colors.go
+++ colors/colors.go
@@ -1,6 +1,7 @@
 package colors
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"math/rand"
@@ -30,9 +31,52 @@ func Rotate(session *discordgo.Session, delayMs int) {
 func changeColor(session *discordgo.Session, guildRole *guildroles.GuildRole) error {
 	color := rand.Intn(maxColor)
 
-	_, err := session.GuildRoleEdit(guildRole.GuildID, guildRole.ID, guildRole.Name, color, guildRole.Hoist, guildRole.Permissions, guildRole.Mentionable)
+	_, err := guildRoleEdit(session, guildRole.GuildID, guildRole.ID, guildRole.Name, color, guildRole.Hoist, guildRole.Permissions, guildRole.Mentionable)
 	if err != nil {
 		return fmt.Errorf("error updating role color for role ID %s, guild ID %s: %w", guildRole.ID, guildRole.GuildID, err)
+	}
+
+	log.Printf("changed color for role ID %s, guild ID %s to %d\n", guildRole.ID, guildRole.GuildID, color)
+
+	return nil
+}
+
+func guildRoleEdit(s *discordgo.Session, guildID, roleID, name string, color int, hoist bool, perm int64, mention bool) (st *discordgo.Role, err error) {
+	// copied from discordgo/restapi.go
+	// modified to return error when request fails
+
+	// Prevent sending a color int that is too big.
+	if color > 0xFFFFFF {
+		err = fmt.Errorf("color value cannot be larger than 0xFFFFFF")
+		return nil, err
+	}
+
+	data := struct {
+		Name        string `json:"name"`               // The role's name (overwrites existing)
+		Color       int    `json:"color"`              // The color the role should have (as a decimal, not hex)
+		Hoist       bool   `json:"hoist"`              // Whether to display the role's users separately
+		Permissions int64  `json:"permissions,string"` // The overall permissions number of the role (overwrites existing)
+		Mentionable bool   `json:"mentionable"`        // Whether this role is mentionable
+	}{name, color, hoist, perm, mention}
+
+	body, err := s.RequestWithBucketID("PATCH", discordgo.EndpointGuildRole(guildID, roleID), data, discordgo.EndpointGuildRole(guildID, ""))
+	if err != nil {
+		return nil, fmt.Errorf("error sending request: %w", err)
+	}
+
+	err = unmarshal(body, &st)
+	if err != nil {
+		return nil, fmt.Errorf("error unmarshalling response: %w", err)
+	}
+
+	return
+}
+
+func unmarshal(data []byte, v interface{}) error {
+	// copied from discordgo/restapi.go
+	err := json.Unmarshal(data, v)
+	if err != nil {
+		return err
 	}
 
 	return nil
